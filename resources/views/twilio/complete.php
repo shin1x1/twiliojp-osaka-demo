0cdd9af2ff443bdec5868694670e7ec152469c8e
shin1x1 <shin1x1@gmail.com>
2015-09-30 01:40:38 +0900

    Fix xml

--- resources/views/twilio/complete.php
+++ resources/views/twilio/complete.php
@@ -1,4 +1,4 @@
-<? xml version = "1.0" encoding = "UTF-8"?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
 <Response>
     <Say voice="women" language="ja-JP">完了しました。電話を切って下さい。</Say>
 </Response>
--- resources/views/twilio/response.php
+++ resources/views/twilio/response.php
@@ -1,4 +1,4 @@
-<? xml version = "1.0" encoding = "UTF-8"?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
 <Response>
     <Say voice="man" language="ja-JP">ようこそ！TwilioUG-Osakaへ。楽しんで下さいね。</Say>
     <Gather timeout="10" numDigits="1" action="<?= url('/gathering') ?>">
